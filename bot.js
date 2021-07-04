b5d8698b5db8dbfe1470bd8b8ce825e357a3240d
xo9991 <86898054+xo9991@users.noreply.github.com>
2021-07-05 01:38:07 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,8 +18,6 @@ const {Message, StringSession, Image, Video} = require('./whatsasena/');
 const { DataTypes } = require('sequelize');
 const { GreetingsDB, getMessage } = require("./plugins/sql/greetings");
 const got = require('got');
-const simpleGit = require('simple-git');
-const git = simpleGit();
 
 const heroku = new Heroku({
     token: config.HEROKU.API_KEY
